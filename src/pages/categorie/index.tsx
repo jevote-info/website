3ea9a2105db955e36e80590990a3accd17f2c9a8
Alan Chauchet <alan.chauchet@gmail.com>
2022-01-22 16:31:42 +0100

    feat: redirects to first category

--- src/pages/categorie/index.tsx
+++ src/pages/categorie/index.tsx
@@ -1,12 +1,10 @@
-import { Box, Button, Container, Heading } from '@chakra-ui/react';
-import React from 'react';
+import { useEffect } from 'react';
 import type { GetStaticProps } from 'next';
-import Head from 'next/head';
-import NextLink from 'next/link';
 import { useMemo } from 'react';
 import superjson from 'superjson';
 import { fetchSurvey } from '../../services/survey';
 import Survey from '../../types/survey';
+import { useRouter } from 'next/router';
 
 interface SerialiazedCategoryProps {
   survey: string;
@@ -25,47 +23,18 @@ export const getStaticProps: GetStaticProps<SerialiazedCategoryProps> = async ({
 };
 
 const Survey = (serializedProps: SerialiazedCategoryProps) => {
+  const { replace } = useRouter();
+
   const survey = useMemo(
     () => superjson.parse<Survey>(serializedProps.survey),
     [serializedProps.survey],
   );
 
-  return (
-    <Box w="100%">
-      <Head>
-        <title>JeVote</title>
-        <meta
-          name="description"
-          content="Découvrez quel candidat(e) est le plus proche de vos convictions grace à un questionnaire sur les programmes des candidats"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  useEffect(() => {
+    replace(`/categorie/${survey[0].slug}`);
+  }, [replace]);
 
-      <Container
-        h="full"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        rowGap={4}
-      >
-        <Heading as="h1" _hover={{ fontSize: 44 }} transition="all 200ms ease">
-          Survey
-        </Heading>
-        <NextLink href={`/categorie/${survey[0].slug}`} passHref>
-          <Button
-            as="a"
-            colorScheme="blue"
-            aria-label="Démarrer le questionnaire"
-            marginY="2px"
-            width="100%"
-          >
-            Démarrer le questionnaire
-          </Button>
-        </NextLink>
-      </Container>
-    </Box>
-  );
+  return null;
 };
 
 export default Survey;
