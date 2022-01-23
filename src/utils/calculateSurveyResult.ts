db1cd80b8b0a937dc8bf96e9a81d818d0d70ea49
Baptiste Acca <acca.baptiste@gmail.com>
2022-01-23 18:51:01 +0100

    add factors calculation

--- src/utils/calculateSurveyResult.ts
+++ src/utils/calculateSurveyResult.ts
@@ -1,21 +1,23 @@
 import groupBy from 'lodash/groupBy';
 import sum from 'lodash/sum';
+import max from 'lodash/max';
+import min from 'lodash/min';
 import Answers from '../types/answers';
 import Survey from '../types/survey';
 import { SurveyResult, SurveyResultScore } from '../types/surveyResult';
 
 export function calculateSurveyResult(survey: Survey, answers: Answers): SurveyResult {
-  const categoriesScores = survey.map(category => {
-    const questionScores = category.questions.map(question => {
-      const answer = answers[category.id][question.id];
-      const selectedChoice = question.choices.find(choice => choice.id === answer.choiceId);
+  const categoriesScores = survey.map(({ id: categoryId, questions }) => {
+    const questionScores = questions.map(({ id: questionId, choices }) => {
+      const answer = answers[categoryId][questionId];
+      const selectedChoice = choices.find(choice => choice.id === answer.choiceId);
 
       if (!selectedChoice) {
         throw `Couldn't find choice ${answer.choiceId}`;
       }
 
       return {
-        questionId: question.id,
+        questionId,
         choiceId: answer.choiceId,
         scores: selectedChoice.politicianScores.map(({ score, politicianId }) => ({
           politicianId,
@@ -25,7 +27,7 @@ export function calculateSurveyResult(survey: Survey, answers: Answers): SurveyR
     });
 
     return {
-      categoryId: category.id,
+      categoryId,
       questionScores,
       scores: groupPoliticianScores(questionScores),
     };
@@ -49,4 +51,41 @@ const groupPoliticianScores = (answers: { scores: SurveyResultScore[] }[]): Surv
       score: sum(scores.map(e => e?.score)),
     }))
     .sort((a, b) => a.score - b.score);
+};
+
+const calculatePoliticianFactor = (survey: Survey) => {
+  survey.map(category => {
+    const questionScores = category.questions.map(({ id: questionId, choices }) => {
+      const politicianScores = choices.flatMap(({ politicianScores }) =>
+        politicianScores.map(({ politicianId, score }) => ({ politicianId, score })),
+      );
+
+      const possibleScoresByPolitician = Object.entries(
+        groupBy(politicianScores, 'politicianId'),
+      ).map(([politicianId, scores]) => ({
+        politicianId,
+        minPossibleScore: min(scores.map(e => e.score)),
+        maxPossibleScore: max(scores.map(e => e.score)),
+      }));
+
+      return {
+        questionId,
+        possibleScoresByPolitician,
+      };
+    });
+
+    const possibleScoresByPolitician = Object.entries(
+      groupBy(politicianScores, 'politicianId'),
+    ).map(([politicianId, scores]) => ({
+      politicianId,
+      minPossibleScore: min(scores.map(e => e.score)),
+      maxPossibleScore: max(scores.map(e => e.score)),
+    }));
+
+    return {
+      categoryId: category.id,
+      questionScores,
+      scores: groupPoliticianScores(questionScores),
+    };
+  });
 };
