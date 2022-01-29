18ba45d65b33c4c5fa8ac7995f49ae8a46bd525c
Alan Chauchet <alan.chauchet@gmail.com>
2022-01-29 18:51:39 +0100

    feat: remove cypress

--- cypress/integration/home.spec.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe(`Homepage`, () => {
-  it(`should render other button`, () => {
-    cy.visit('/');
-
-    cy.get('button').contains('Go to other').should('be.visible');
-  });
-});
