7359d0eaee87a974a5735e08a4d06657ae0feafb
Alan Chauchet <alan.chauchet@gmail.com>
2022-01-30 17:16:56 +0100

    feat: get politicians

--- src/pages/resultats.tsx
+++ src/pages/resultats.tsx
@@ -1,8 +1,10 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
+import { Politician } from '@prisma/client';
 import { GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect, useMemo } from 'react';
 import superjson from 'superjson';
+import { fetchPoliticians } from '../services/politicians';
 import { fetchSurvey } from '../services/survey';
 import { useSurveyStore } from '../stores/survey';
 import { Survey, SurveyPoliticiansPossibleScores } from '../types/survey';
@@ -10,6 +12,7 @@ import { calculatePoliticianFactor } from '../utils/calculatePoliticianPossibleS
 
 interface SerializedResultsProps {
   survey: string;
+  politicians: string;
   politicianPossibleScores: SurveyPoliticiansPossibleScores;
 }
 
@@ -17,11 +20,15 @@ export const getStaticProps: GetStaticProps<SerializedResultsProps> = async ({
   preview = false,
 }) => {
   const survey = await fetchSurvey({ previewMode: preview });
+  const politicians = await fetchPoliticians();
   const politicianPossibleScores = calculatePoliticianFactor(survey);
 
   return {
     props: {
       survey: superjson.stringify(survey),
+      politicians: superjson.stringify(
+        politicians.reduce((acc, politician) => ({ ...acc, [politician.id]: politician }), {}),
+      ),
       politicianPossibleScores,
     },
   };
@@ -35,6 +42,11 @@ function ResultsPage(serializedProps: SerializedResultsProps) {
   const survey = useMemo(
     () => superjson.parse<Survey>(serializedProps.survey),
     [serializedProps.survey],
+  );
+
+  const politicians = useMemo(
+    () => superjson.parse<Record<Politician['id'], Politician>>(serializedProps.politicians),
+    [serializedProps.politicians],
   );
 
   const { calculateResult, findMissingAnswer } = useSurveyStore();
@@ -54,8 +66,20 @@ function ResultsPage(serializedProps: SerializedResultsProps) {
   }, [survey, politicianPossibleScores, calculateResult]);
 
   console.log(results);
+  console.log(politicians);
 
-  return <Box>{JSON.stringify(results)}</Box>;
+  const favPolitician = results.scores[0] && politicians[results.scores[0].politicianId];
+
+  if (!favPolitician) {
+    return null;
+  }
+
+  return (
+    <Box>
+      {favPolitician.name}
+      <Image src={favPolitician.pictureUrl} alt={favPolitician.name} />
+    </Box>
+  );
 }
 
 export default ResultsPage;
