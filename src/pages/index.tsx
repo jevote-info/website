bad1a2b4c54992840f9622f0329401c134983ed9
Simon <simon.galet@gmail.com>
2022-01-30 18:36:20 +0100

    feat(front): Basic home header
    Basic home header with an image and a start survey button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import { Box, Container, Heading } from '@chakra-ui/react';
+import { Box, Button, Container, Heading } from '@chakra-ui/react';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import NextLink from 'next/link';
 import { HomeLayout } from '../components/HomeLayout';
+import { useIsMobile } from '../hooks/useIsMobile';
 import { fetchSurvey } from '../services/survey';
 
 interface HomeProps {
@@ -21,6 +23,7 @@ export const getStaticProps: GetStaticProps<HomeProps> = async ({ preview = fals
 };
 
 function Home(props: HomeProps) {
+  const isMobile = useIsMobile();
   const { surveyPath } = props;
 
   return (
@@ -34,20 +37,54 @@ function Home(props: HomeProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomeLayout surveyPath={surveyPath}>
-        <Box w="100%">
-          <Container
-            h="full"
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            rowGap={4}
+        <Container h="full" maxW="container.xl">
+          <Box
+            position="relative"
+            overflow="hidden"
+            backgroundImage="/home-banner.jpeg"
+            backgroundSize="contain"
+            backgroundRepeat="no-repeat"
+            width="100%"
+            height={0}
+            paddingTop="66.67%"
+            borderRadius="lg"
           >
-            <Heading _hover={{ fontSize: 44 }} transition="all 200ms ease">
-              Accueil
-            </Heading>
-          </Container>
-        </Box>
+            <Box
+              position="absolute"
+              top="0"
+              right="0"
+              bottom="0"
+              left="0"
+              display="flex"
+              flexDirection="column"
+              justifyContent="flex-end"
+              alignItems="center"
+              padding={isMobile ? '16px' : '32px'}
+            >
+              <Heading
+                as="h1"
+                size="lg"
+                textAlign="center"
+                color="primary.200"
+                marginLeft="128px"
+                marginRight="128px"
+                marginBottom="32px"
+              >
+                Magnifique phrase d&apos;accroche
+              </Heading>
+              <NextLink href={surveyPath} passHref>
+                <Button
+                  as="a"
+                  backgroundColor="primary.200"
+                  color="black"
+                  aria-label="Lancer le questionnaire"
+                >
+                  Lancer le questionnaire
+                </Button>
+              </NextLink>
+            </Box>
+          </Box>
+        </Container>
       </HomeLayout>
     </>
   );
