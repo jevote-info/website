f98ccfdbc1058f18d8bdd5e3a7ecd81e2d8f2ddc
Simon <simon.galet@gmail.com>
2022-02-28 23:15:23 +0100

    Add team message and add dark theme favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,8 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
           <link rel="manifest" href="/site.webmanifest"></link>
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4c4fb6"></link>
           <meta name="msapplication-TileColor" content="#da532c"></meta>
