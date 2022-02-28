1befb785d6ac5517d2ce1a0fc54e2a31c1fffe0b
Alan Chauchet <alan.chauchet@gmail.com>
2022-02-28 23:53:17 +0100

    feat: removes noindex

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,6 @@ class MyDocument extends Document {
     return (
       <Html lang="fr">
         <Head>
-          <meta name="robots" content="noindex"></meta>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
